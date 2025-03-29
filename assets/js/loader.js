535f529ee829e38ae28b522b9f406c61a2f74c1b
Mani19492 <23eg509a04@anurag.edu.in>
2025-03-29 20:59:03 +0530

    hack attack

---
+++ assets/js/loader.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', () => {
+    const loader = document.getElementById('loader');
+    setTimeout(() => {
+      loader.classList.add('fade-out');
+    }, 2500); // Optional delay for smoothness
+  });
+  
