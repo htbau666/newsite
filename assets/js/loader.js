566ab681de3d4c103b26978c182c34f9c4cc479d
S. Poorna Mani Teja <spmteja09@gmail.com>
2025-12-25 19:03:23 +0530

    Remove Tailwind CSS styles and related imports from the project

--- assets/js/loader.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', () => {
-    const loader = document.getElementById('loader');
-    setTimeout(() => {
-      loader.classList.add('fade-out');
-    },4500 ); // Optional delay for smoothness
-  });
-  
